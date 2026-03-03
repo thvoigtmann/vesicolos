c259d12275f80175efca89b45017aeb6897d015d
VESICOLOS <vesicolos@esrange>
2026-03-03 19:17:54 +0100

    Dev version of start script, not intended for main!

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/Desktop/vesicolos
 source venv/bin/activate
-python3 ./vesicolos.py
+#python3 ./vesicolos.py
 # the following to keep the heater off when program exits
 python3 ./tests/heater.py
