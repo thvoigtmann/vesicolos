d054e5ef244f5d0acfbf066fe5a2cc37a887850a
VESICOLOS <vesicolos@vesicolos.local>
2026-09-23 11:24:39 +0200

    Fixed stupid typo.

--- vesicolos_utils/cli.py
+++ vesicolos_utils/cli.py
@@ -59,7 +59,7 @@ class CLI:
         self.monitor.statusbar = self._print_bar
         return self
     def __exit__ (self, exc_type, exc_value, traceback):
-        if setl.stop and self.camera:
+        if self.stop and self.camera:
             self.camera.stop()
             self.camera = None
         if self.videostream:
